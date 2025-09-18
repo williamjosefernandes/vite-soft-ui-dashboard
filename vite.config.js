9d1434b1caedd94923d9764dbef82b09dff86083
William Fernandes <williamjosefernandes@gmail.com>
2025-09-18 16:48:52 -0300

    Fix vite.config.js syntax for compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
-    host: "0.0.0.0",
-    port: 8080,
-  },
-});
+    host: '0.0.0.0',
+    port: 8080
+  }
+})
